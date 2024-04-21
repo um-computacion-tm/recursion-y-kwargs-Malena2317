b45f4c15b2a8603596bcbaaacaec4cffa1b14faa
Malena2317 <me.navarro@alumno.um.edu.ar>
2024-04-21 12:12:47 -0300

    Correccion de errores

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,10 @@
 import unittest
 def fibonacci(n):
-    if n == (0,1):
+    if n <= 1:
         return n
     else:
-        fibonacci(n-1)+fibonacci(n-2)
-    #Suerte!
+        return fibonacci(n-1) + fibonacci(n-2)
+
     
     
 class TestFibonacci(unittest.TestCase):
@@ -18,21 +18,31 @@ class TestFibonacci(unittest.TestCase):
     
     def test_with_2(self):
         resultado = fibonacci(2)
+        self.assertEqual(resultado, 1)
+
+    def test_with_3(self):
+        resultado = fibonacci(3)
         self.assertEqual(resultado, 2)
-    
-    def test_with_2(self):
-        resultado = fibonacci(2)
-        self.assertEqual(resultado, 2)
-    
+
+    def test_with_4(self):
+        resultado = fibonacci(4)
+        self.assertEqual(resultado, 3)
+
     def test_with_5(self):
         resultado = fibonacci(5)
-        self.assertEqual(resultado, 5)
+        self.assertEqual(resultado, 5) 
     
-    def test_with_6(self):
-        resultado = fibonacci(6)
-        self.assertEqual(resultado, 8)
-    
+    def test_with_10(self):
+        resultado = fibonacci(10)
+        self.assertEqual(resultado, 55) 
 
+    def test_with_13(self):
+        resultado = fibonacci(13)
+        self.assertEqual(resultado, 233)
+
+    def test_with_17(self):
+        resultado = fibonacci(17)
+        self.assertEqual(resultado, 1597)
 
 
 unittest.main()
